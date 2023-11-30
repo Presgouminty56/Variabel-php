8268922ec1097d7d161a4db8956634612161ce4c
Andrian43 <117629945+Andrian43@users.noreply.github.com>
2023-12-01 03:08:08 +0700

    variabelphp

---
+++ Contoh variabel.php
@@ -0,0 +1,12 @@
+<?php
+
+
+ $namaHero = "Bimo"; //contoh buat variebel hehehe 
+ $damage  = 200; // contoh buat variabel 
+
+
+
+
+ echo " nama heru gua : "  .$namaHero. " damage : ". $damage; //implementasi 
+
+ ?>
---
+++ latihan1.php
@@ -0,0 +1,8 @@
+<?php
+
+$uangSaya = 200; // variabelnya dan value
+
+echo "100 + 100:" .$uangSaya; // implementasinya 
+
+
+?>
